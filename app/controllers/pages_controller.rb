26a176978e186c6d9b5fbb159604b24355d93d7e
wtung008 <wayne.h.tung@gmail.com>
2017-01-30 15:15:18 -0500

    defining @header instance variable in controller page for welcome, about, and contest

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,13 +1,15 @@
 class PagesController < ApplicationController
 
   def welcome
-    render :welcome
+    @header = "This is the welcome page2"
   end
 
   def about
+    @header = "ABOUT PAGE"
   end
 
   def contest
+    @header = "CONTEST"
   end
 
 end
